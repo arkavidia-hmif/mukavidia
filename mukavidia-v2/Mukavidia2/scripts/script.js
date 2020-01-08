4abd7d4401af5b18b9899620c521d3d4e82937f5
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2020-01-08 17:18:03 +0700

    Deleted options D and E

--- mukavidia-v2/Mukavidia2/scripts/script.js
+++ mukavidia-v2/Mukavidia2/scripts/script.js
@@ -5,8 +5,8 @@ const Patches = require('Patches');
 const texture1 = Textures.get('option-a'); 
 const texture2 = Textures.get('option-b'); 
 const texture3 = Textures.get('option-c'); 
-const texture4 = Textures.get('option-d'); 
-const texture5 = Textures.get('option-e'); 
+// const texture4 = Textures.get('option-d'); 
+// const texture5 = Textures.get('option-e'); 
 
 const picker = NativeUI.picker; 
 
@@ -18,8 +18,8 @@ const configuration = {
     {image_texture: texture1}, 
     {image_texture: texture2}, 
     {image_texture: texture3}, 
-    {image_texture: texture4}, 
-    {image_texture: texture5}
+    // {image_texture: texture4}, 
+    // {image_texture: texture5}
   ]
 };
 
